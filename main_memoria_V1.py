cc386bdcc6797a255b5c3e4a9acb294b5d69dab2
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-14 20:51:22 -0300

    Update main_memoria_V1.py

--- main_memoria_V1.py
+++ main_memoria_V1.py
@@ -47,8 +47,30 @@ while True:
 
     opcao = input('\nDigita um número: ')
     print(lista_tabela[0][0])
+
     if opcao == cobertura[0][0]:
         cobertura[0][0] = str(lista_tabela[0][0])
+
+    elif opcao == cobertura[0][0]:
+        cobertura[0][0] = str(lista_tabela[0][1]
+
+    elif opcao == cobertura[0][1]:
+        cobertura[0][0] = str(lista_tabela[1][0]
+
+    elif opcao == cobertura[1][0]:
+        cobertura[0][0] = str(lista_tabela[1][1]
+
+    elif opcao == cobertura[1][1]:
+        cobertura[0][0] = str(lista_tabela[2][0]
+
+    elif opcao == cobertura[2][0]:
+        cobertura[0][0] = str(lista_tabela[2][1]
+
+    elif opcao == cobertura[2][1]:
+        cobertura[0][0] = str(lista_tabela[3][0]
+
+    elif opcao == cobertura[3][0]:
+        cobertura[0][0] = str(lista_tabela[3][1]
 
 
 
