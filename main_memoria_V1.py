d92dd4a9f84e197a43fd4439e01972c252355229
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-13 22:30:35 -0300

    Update main_memoria_V1.py

--- main_memoria_V1.py
+++ main_memoria_V1.py
@@ -6,8 +6,14 @@ possibilidade = ['A', 'B', 'C', 'E', 'F', 'G', 'H', 'I', 'J', 'L']
 
 primeira_escolha = random.choice(possibilidade)
 segunda_escolha = random.choice(possibilidade)
+terceira_escolha = random.choice(possibilidade)
+
 if primeira_escolha == segunda_escolha:
     segunda_escolha = random.choice(possibilidade)
+if primeira_escolha and segunda_escolha == terceira_escolha:
+    terceira_escolha = random.choice(possibilidade)
 
-tabela = [primeira_escolha, segunda_escolha]
-print(tabela)
+tabela = [[segunda_escolha], [primeira_escolha], [segunda_escolha], [primeira_escolha]]
+for i in tabela:
+    for y in i:
+        print(i, y)
