09abf9a4703db818ce3a50ea94f1154cd0ac972c
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-13 22:18:40 -0300

    Update main_memoria_V1.py

--- main_memoria_V1.py
+++ main_memoria_V1.py
@@ -1,4 +1,14 @@
 import random
 
-computador = list()
+tabela = [[]]
+
+possibilidade = ['A', 'B', 'C', 'E', 'F', 'G', 'H', 'I', 'J', 'L']
+
+primeira_escolha = random.choice(possibilidade)
+sengunda_escolha = random.choice(possibilidade)
+if primeira_escolha == sengunda_escolha:
+    sengunda_escolha = random.choice(possibilidade)
+
+print(primeira_escolha)
+print(sengunda_escolha)
 
