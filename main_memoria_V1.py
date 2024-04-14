bdac82000a46e0ff12e4ee35dba0dbe1d7850cee
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-14 15:50:15 -0300

    Update main_memoria_V1.py

--- main_memoria_V1.py
+++ main_memoria_V1.py
@@ -18,19 +18,25 @@ if primeira_escolha and segunda_escolha == terceira_escolha:
 if primeira_escolha and segunda_escolha and terceira_escolha == quarta_escolha:
     quarta_escolha = random.choice(possibilidade)
 
+lista_tabela = [primeira_escolha, primeira_escolha, segunda_escolha, segunda_escolha,
+                terceira_escolha, terceira_escolha, quarta_escolha, quarta_escolha]
+print(lista_tabela)
 
-tabela[0][0] = primeira_escolha
-tabela[0][1] = primeira_escolha
-tabela[1][0] = segunda_escolha
-tabela[1][1] = segunda_escolha
-tabela[2][0] = terceira_escolha
-tabela[2][1] = terceira_escolha
-tabela[3][0] = quarta_escolha
-tabela[3][1] = quarta_escolha
+for i in range(0, 1):
+    tabela[0][i] = primeira_escolha
+    tabela[0][i] = primeira_escolha
+    tabela[1][i] = segunda_escolha
+    tabela[1][i] = segunda_escolha
+    tabela[2][i] = terceira_escolha
+    tabela[2][i] = terceira_escolha
+    tabela[3][i] = quarta_escolha
+    tabela[3][i] = quarta_escolha
 
-tabela.reverse()
+random.shuffle(lista_tabela)
 
-for i in tabela:
+print(tabela)
+
+for i in lista_tabela:
     print()
     for y in i:
         print(f'[ {y} ]', end=' ')
