95fc0e606f756527dd42bda438204bdcf4654e7f
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-13 22:41:47 -0300

    Update main_memoria_V1.py

--- main_memoria_V1.py
+++ main_memoria_V1.py
@@ -1,19 +1,37 @@
 import random
 
-tabela = []
+tabela = [[0, 0], [0, 0], [0, 0], [0, 0]]
 
 possibilidade = ['A', 'B', 'C', 'E', 'F', 'G', 'H', 'I', 'J', 'L']
 
 primeira_escolha = random.choice(possibilidade)
 segunda_escolha = random.choice(possibilidade)
 terceira_escolha = random.choice(possibilidade)
+quarta_escolha = random.choice(possibilidade)
 
 if primeira_escolha == segunda_escolha:
     segunda_escolha = random.choice(possibilidade)
 if primeira_escolha and segunda_escolha == terceira_escolha:
     terceira_escolha = random.choice(possibilidade)
+if primeira_escolha and segunda_escolha and terceira_escolha == quarta_escolha:
+    quarta_escolha = random.choice(possibilidade)
 
-tabela = [[segunda_escolha], [primeira_escolha], [segunda_escolha], [primeira_escolha]]
+print(primeira_escolha)
+print(segunda_escolha)
+print(terceira_escolha)
+print(quarta_escolha)
+
+tabela[0][0] = primeira_escolha
+tabela[0][1] = primeira_escolha
+tabela[1][0] = segunda_escolha
+tabela[1][1] = segunda_escolha
+tabela[2][0] = terceira_escolha
+tabela[2][1] = terceira_escolha
+tabela[3][0] = quarta_escolha
+tabela[3][1] = quarta_escolha
+
+
+
 for i in tabela:
     for y in i:
         print(i, y)
