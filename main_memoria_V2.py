ef44f8e7f1e74b2f5ee54a2edfe35b4f2fa764e6
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-18 10:24:57 -0300

    Update main_memoria_V2.py

--- main_memoria_V2.py
+++ main_memoria_V2.py
@@ -7,7 +7,7 @@ lista_de_emojes = []
 def leia_int(valor):
     while True:
         try:
-            return input(int(valor))
+            return int(valor)
         except:
             print(f'Opção INVALIDA!, Digite novamente')
 
@@ -18,7 +18,7 @@ while True:
     [2] Ver pontuação
     [0] Sair
     ''')
-    resp_opcao_menu = leia_int('Escolha uma OPÇÂO: ')
+    resp_opcao_menu = leia_int(input('Escolha uma OPÇÂO: '))
     if resp_opcao_menu == 1:
         pass
     elif resp_opcao_menu == 2:
