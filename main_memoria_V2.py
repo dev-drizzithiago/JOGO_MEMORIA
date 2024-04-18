943cd1876d54e8c56b743cb28969081a40f7a666
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-04-18 10:20:40 -0300

    Update main_memoria_V2.py

---
+++ main_memoria_V2.py
@@ -0,0 +1,28 @@
+import random
+from time import sleep
+
+lista_de_emojes = []
+
+
+def leia_int(valor):
+    try:
+        return input(int(valor))
+    except:
+        print(f'Opção INVALIDA!, Digite novamente')
+
+
+while True:
+    print('''
+    [1] Iniciar jogo
+    [2] Ver pontuação
+    [0] Sair
+    ''')
+    resp_opcao_menu = leia_int('Escolha uma OPÇÂO: ')
+    if resp_opcao_menu == 1:
+        pass
+    elif resp_opcao_menu == 2:
+        pass
+    elif resp_opcao_menu == 0:
+        print('Fechando o Programa')
+        sleep(1)
+        break
