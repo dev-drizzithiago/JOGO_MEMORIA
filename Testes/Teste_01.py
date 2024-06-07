d705af7068b4a20c552205b9e1d9d3c88c2ecb0d
Thiago Alves Pinheiro <th_grifon@hotmail.com>
2024-06-07 20:50:19 -0300

    Create Teste_01.py

---
+++ Testes/Teste_01.py
@@ -0,0 +1,15 @@
+
+
+def cria_matriz(m, n, valor):
+    matriz = []
+    linha = [valor]*n
+    for i in range(m):
+        matriz.append(linha[:])
+    print(matriz)
+
+
+linha = 5
+coluna = 5
+valor = 0
+
+cria_matriz(linha, coluna)
